9b0132926c163c6d3087ac3b77b6f433cfa46bef
Christoph Perger <christoph.perger@spatial-focus.net>
2021-01-22 11:23:48 +0100

    Simplify example

--- src/AuditExample/Address.cs
+++ src/AuditExample/Address.cs
@@ -6,14 +6,8 @@ namespace AuditExample
 {
 	public class Address
 	{
-		public Address(string city, string street)
-		{
-			City = city;
-			Street = street;
-		}
+		public string City { get; set; }
 
-		public string City { get; private set; }
-
-		public string Street { get; private set; }
+		public string Street { get; set; }
 	}
 }
--- src/AuditExample/Program.cs
+++ src/AuditExample/Program.cs
@@ -16,8 +16,12 @@ namespace AuditExample
 			context.Database.EnsureDeleted();
 			context.Database.EnsureCreated();
 
-			context.Departments.Add(new Department() { Id = 1, Name = "Development", Address = new Address("Vienna", "First Street") });
+			context.Departments.Add(new Department()
+			{
+				Id = 1, Name = "Development", Address = new Address { City = "Vienna", Street = "First Street" },
+			});
 
+			// ArgumentNullException will be thrown here: Value cannot be null. (Parameter 'name')
 			context.SaveChanges();
 		}
 	}
